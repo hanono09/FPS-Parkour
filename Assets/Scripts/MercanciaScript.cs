90021b88ff5b0f78b1087c883fd52ff9067d109e
hanono09 <fedehanono@gmail.com>
2025-05-29 11:59:40 -0300

    collision y destruccion terminado.

---
+++ Assets/Scripts/MercanciaScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MercanciaScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Player")
+        {
+            Destroy(gameObject);
+
+        }
+    }
+}
