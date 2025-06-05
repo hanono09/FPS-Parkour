e6eb818cd91116206fca5e1662c6a4896b87d19a
hanono09 <fedehanono@gmail.com>
2025-06-05 12:17:42 -0300

    notealcanzacasiterminado.

--- Assets/Scripts/MercanciaScript.cs
+++ Assets/Scripts/MercanciaScript.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class MercanciaScript : MonoBehaviour
 {
+    public MoneyManager moneyManager;
+    public float cost;
     // Start is called before the first frame update
+
+    private void Start()
+    {
+        moneyManager = FindObjectOfType<MoneyManager>();
+    }
+
+
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.name == "Player")
         {
+            //actualizar el dinero por compra
+            moneyManager.UpdateMoney(-cost);    
             Destroy(gameObject);
 
         }
